d008ce98d176dd0440d38d137e3d4252fa876cd7
Philip-Hart <71290300+Philip-Hart@users.noreply.github.com>
2024-03-20 22:19:00 -0400

    initial songbyrd

---
+++ src/scrapers/songbyrd/config.ts
@@ -0,0 +1,9 @@
+import { ScraperConfig } from "../../types";
+
+export const config: ScraperConfig = {
+  id: "aTOmu1pdEuE7atxQSeJL",
+  name: "song-byrd",
+  url: "https://songbyrddc.com/",
+  sitemap: "https://songbyrddc.com/event_listing-sitemap.xml",
+  city: "Washington, DC",
+};
